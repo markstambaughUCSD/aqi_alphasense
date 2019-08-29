398e6d3bcfa193d29edf44b4476426995f38e525
markstambaughUCSD <mstambaugh@ucsd.edu>
2019-08-29 23:24:06 +0000

    added http and location to config

--- aqi_main.py
+++ aqi_main.py
@@ -12,6 +12,7 @@ import PM
 import TMP36
 from time import time
 import sqlite3
+import requests
 
 test_ADC = False
 
@@ -45,17 +46,17 @@ if test_ADC:
 	print mux_ADC.LUT_bits
 	
 	while True:
-	        channel = int(raw_input("channel?: "))
-        	analog_mux.select_channel(channel)
+		channel = int(raw_input("channel?: "))
+		analog_mux.select_channel(channel)
 		print mux_ADC.read("bits")
 		print mux_ADC.value_mV
 
 # set up the PM2.5, PM10 sensor
 sensor_PM = PM.PM(pins.PM_2U5_PWM, pins.PM_10U_PWM)
 if sensor_PM.concentration_2u5_ugpm3 != -1 and sensor_PM.concentration_10u_ugpm3 != -1:
-    print "PM 2u5, 10u sensor initialized"
+	print "PM 2u5, 10u sensor initialized"
 else:
-    print "PM 2u5, 10u sensor failure"
+	print "PM 2u5, 10u sensor failure"
 
 # load calibration data for each gas sensor
 sensor_NO2 = gas_sensor.GasSensor("NO2", cal.NO2)
@@ -65,64 +66,78 @@ sensor_O3 = gas_sensor.GasSensor("O3", cal.O3)
 print "gas sensors calibration loaded"
 
 # initialize data frame
-aqi_frame = {"time_s": 0, "temp_C": 25, "NO2_ppb": 0, "SO2_ppb": 0, "CO_ppb": 0, "O3_ppb": 0, "PM_2u5_ugpm3": 0, "PM_10u_ugpm3": 0, "aqi": 0, "critical pollutant": "none"}
+aqi_frame = {"time_s": 0, "temp_C": 25, "NO2_ppb": 0, "SO2_ppb": 0, "CO_ppb": 0, "O3_ppb": 0, "PM_2u5_ugpm3": 0, "PM_10u_ugpm3": 0}
 
 # setup database
 conn = sqlite3.connect("aqi.db")
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS history (time INT PRIMARY KEY NOT NULL, temp INT, NO2_ppb INT, SO2_ppb INT, CO_ppb INT, O3_ppb INT, PM_2u5_ugpm3 INT, PM_10_ugpm3 INT, aqi INT, crit_pol STRING)")
+c.execute("CREATE TABLE IF NOT EXISTS history (time_s INT PRIMARY KEY NOT NULL, temp INT, NO2_ppb INT, SO2_ppb INT, CO_ppb INT, O3_ppb INT, PM_2u5_ugpm3 INT, PM_10_ugpm3 INT)")
 conn.close()
 
 
+# save one set of data to the local database
 def save_frame_to_db(f):
-    frame = (f["time_s"], f["temp_C"], f["NO2_ppb"], f["SO2_ppb"], f["CO_ppb"], f["O3_ppb"], f["PM_2u5_ugpm3"], f["PM_10u_ugpm3"], f["aqi"], f["critical pollutant"])
-    conn = sqlite3.connect("aqi.db")
-    c = conn.cursor()
-    c.execute("INSERT INTO history VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", frame)
-    conn.commit()
-    conn.close()
+	frame = (f["time_s"], f["temp_C"], f["NO2_ppb"], f["SO2_ppb"], f["CO_ppb"], f["O3_ppb"], f["PM_2u5_ugpm3"], f["PM_10u_ugpm3"])
+	conn = sqlite3.connect("aqi.db")
+	c = conn.cursor()
+	c.execute("INSERT INTO history VALUES(?, ?, ?, ?, ?, ?, ?, ?)", frame)
+	conn.commit()
+	conn.close()
+
+
+# retrieve data from specified time period and send to the web server over http
+def upload_to_server(start_time, stop_time):
+	conn = sqlite3.connect("aqi.db")
+	c = conn.cursor()
+	c.execute("SELECT * FROM history WHERE time_s BETWEEN ? AND ?", (start_time, stop_time))
+	rows = c.fetchall()
+	for row in rows:
+		frame = {"time_s": row[0], "temp_C": row[1], "NO2_ppb": row[2], "SO2_ppb": row[3], "CO_ppb": row[4], "O3_ppb": row[5], "PM_2u5_ugpm3": row[6], "PM_10u_ugpm3": row[7]}
+#		r = requests.post(url = config.API_ENDPOINT, data = frame}
+#		print r
+#		print row
+#		print frame
 
 
 # gather full set of data
 def read_all_sensors():
-    aqi_frame["time_s"] = int(time())
-
-    # read temperature, C
-    analog_mux.select_channel(pins.CH_TMP36)
-    aqi_frame["temp_C"] = int(TMP36.mV_to_C(mux_ADC.read("mV")))
-
-    # read NO2, ppb
-    analog_mux.select_channel(pins.CH_NO2_WE)
-    sensor_NO2.WE_mV = mux_ADC.read("mV")
-    analog_mux.select_channel(pins.CH_NO2_AE)
-    sensor_NO2.AE_mV = mux_ADC.read("mV")
-    aqi_frame["NO2_ppb"] = int(sensor_NO2.calc_ppb(aqi_frame["temp_C"]))
-
-    # read SO2, ppb
-    analog_mux.select_channel(pins.CH_SO2_WE)
-    sensor_SO2.WE_mV= mux_ADC.read("mV")
-    analog_mux.select_channel(pins.CH_SO2_AE)
-    sensor_SO2.AE_mV = mux_ADC.read("mV")
-    aqi_frame["SO2_ppb"] = int(sensor_SO2.calc_ppb(aqi_frame["temp_C"]))
-
-    # read CO, ppb
-    analog_mux.select_channel(pins.CH_CO_WE)
-    sensor_CO.WE_mV = mux_ADC.read("mV")
-    analog_mux.select_channel(pins.CH_CO_AE)
-    sensor_CO.AE_mV = mux_ADC.read("mV")
-    aqi_frame["CO_ppb"] = int(sensor_CO.calc_ppb(aqi_frame["temp_C"]))
-
-    # read O3, ppb
-    analog_mux.select_channel(pins.CH_O3_WE)
-    sensor_O3.WE_mV = mux_ADC.read("mV")
-    analog_mux.select_channel(pins.CH_O3_AE)
-    sensor_O3.AE_mV = mux_ADC.read("mV")
-    aqi_frame["O3_ppb"] = int(sensor_O3.calc_ppb(aqi_frame["temp_C"]))
-
-    # read PM 2u5 and 10u, ug per m^3
-    aqi_frame["PM_2u5_ugpm3"] = int(round(sensor_PM.read_PM_2u5_ugpm3()))
-    aqi_frame["PM_10u_ugpm3"] = int(round(sensor_PM.read_PM_10u_ugpm3()))
-
+	aqi_frame["time_s"] = int(time())
+	
+	# read temperature, C
+	analog_mux.select_channel(pins.CH_TMP36)
+	aqi_frame["temp_C"] = int(TMP36.mV_to_C(mux_ADC.read("mV")))
+	
+	# read NO2, ppb
+	analog_mux.select_channel(pins.CH_NO2_WE)
+	sensor_NO2.WE_mV = mux_ADC.read("mV")
+	analog_mux.select_channel(pins.CH_NO2_AE)
+	sensor_NO2.AE_mV = mux_ADC.read("mV")
+	aqi_frame["NO2_ppb"] = int(sensor_NO2.calc_ppb(aqi_frame["temp_C"]))
+	
+	# read SO2, ppb
+	analog_mux.select_channel(pins.CH_SO2_WE)
+	sensor_SO2.WE_mV= mux_ADC.read("mV")
+	analog_mux.select_channel(pins.CH_SO2_AE)
+	sensor_SO2.AE_mV = mux_ADC.read("mV")
+	aqi_frame["SO2_ppb"] = int(sensor_SO2.calc_ppb(aqi_frame["temp_C"]))
+	
+	# read CO, ppb
+	analog_mux.select_channel(pins.CH_CO_WE)
+	sensor_CO.WE_mV = mux_ADC.read("mV")
+	analog_mux.select_channel(pins.CH_CO_AE)
+	sensor_CO.AE_mV = mux_ADC.read("mV")
+	aqi_frame["CO_ppb"] = int(sensor_CO.calc_ppb(aqi_frame["temp_C"]))
+	
+	# read O3, ppb
+	analog_mux.select_channel(pins.CH_O3_WE)
+	sensor_O3.WE_mV = mux_ADC.read("mV")
+	analog_mux.select_channel(pins.CH_O3_AE)
+	sensor_O3.AE_mV = mux_ADC.read("mV")
+	aqi_frame["O3_ppb"] = int(sensor_O3.calc_ppb(aqi_frame["temp_C"]))
+	
+	# read PM 2u5 and 10u, ug per m^3
+	aqi_frame["PM_2u5_ugpm3"] = int(round(sensor_PM.read_PM_2u5_ugpm3()))
+	aqi_frame["PM_10u_ugpm3"] = int(round(sensor_PM.read_PM_10u_ugpm3()))
 
 
 start_time_s = time()
@@ -132,23 +147,18 @@ print "begin reading. {0}s sample period, {1}s report period, {2}s run time".for
 print "TIME(s)    TEMP(C) NO2(ppb) SO2(ppb) CO(ppb) O3(ppb) PM2u5(ugpm3) PM10u(ugpm3)"
 
 while time() < start_time_s + config.run_time_s:
-    if time() - last_sample_time_s > config.sample_period_s:
-	last_sample_time_s = time()
-	#print "{0} reading sensors".format(time()-start_time_s)
-        read_all_sensors()
-        save_frame_to_db(aqi_frame)
-	print "%10d %+03d     %04d     %04d     %04d    %04d    %03d          %03d" %( \
-		aqi_frame["time_s"], \
-		aqi_frame["temp_C"], \
-                aqi_frame["NO2_ppb"], \
-                aqi_frame["SO2_ppb"], \
-                aqi_frame["CO_ppb"], \
-                aqi_frame["O3_ppb"], \
-                aqi_frame["PM_2u5_ugpm3"], \
-                aqi_frame["PM_10u_ugpm3"])
-
-    if time() - last_report_time_s > config.report_period_s:
-	print "{0} writing to web server".format(time()-start_time_s)
-        # report all data to web server since last report. Use HTTP
-        last_report_time_s = time()
+	if time() - last_sample_time_s > config.sample_period_s:
+		last_sample_time_s = time()
+		#print "{0} reading sensors".format(time()-start_time_s)
+		read_all_sensors()
+		save_frame_to_db(aqi_frame)
+		print "%10d %+03d     %04d     %04d     %04d    %04d    %03d          %03d" %( \
+		aqi_frame["time_s"], aqi_frame["temp_C"], aqi_frame["NO2_ppb"], aqi_frame["SO2_ppb"], \
+		aqi_frame["CO_ppb"], aqi_frame["O3_ppb"], aqi_frame["PM_2u5_ugpm3"], aqi_frame["PM_10u_ugpm3"])
+	
+	if time() - last_report_time_s > config.report_period_s:
+		print "%d writing to web server" %(time() - start_time_s)
+		# report all data to web server since last report. Use HTTP.
+		upload_to_server(last_report_time_s, time())
+		last_report_time_s = time()
 
--- config.py
+++ config.py
@@ -2,7 +2,15 @@
 # 2019/08/29
 # config file for the air quality sensor board
 
-sample_period_s = 1
+sample_period_s = 3
 report_period_s = 10
 run_time_s = 30
 
+API_ENDPOINT = "http://pastebin.com/api/api_post.php"
+
+API_KEY = "XXXXXXXXXXXXXXXXX"
+
+location = {"text": "Atkinson Hall, UCSD", \
+			"latitude": 32.882454, \
+			"longitude": -117.234696 \
+			}
